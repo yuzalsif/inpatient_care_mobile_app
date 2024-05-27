ce88f6d66960990405946fe3e97d12614b242cc5
yuzalsif <liteit68@gmail.com>
2024-05-27 10:08:33 +0300

    feat: Add password formz input

---
+++ packages/form_fields/lib/src/password.dart
@@ -0,0 +1,24 @@
+import 'package:formz/formz.dart';
+
+class Password extends FormzInput<String, PasswordValidationError> {
+  const Password.unvalidated([
+    String value = '',
+  ]) : super.pure(value);
+
+  const Password.validated([
+    String value = '',
+  ]) : super.dirty(value);
+
+  @override
+  PasswordValidationError? validator(String value) {
+    if (value.isEmpty) {
+      return PasswordValidationError.empty;
+    } else {
+      return null;
+    }
+  }
+}
+
+enum PasswordValidationError {
+  empty,
+}
