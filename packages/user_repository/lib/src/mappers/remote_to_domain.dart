4f43e28c0d6a8015224248a86b3ed535b1da63f8
yuzalsif <liteit68@gmail.com>
2024-06-05 18:04:59 +0300

    Rename method 'toDomain' to be 'toDomainModel'

--- packages/user_repository/lib/src/mappers/remote_to_domain.dart
+++ packages/user_repository/lib/src/mappers/remote_to_domain.dart
@@ -2,7 +2,7 @@ import 'package:domain_models/domain_models.dart';
 import 'package:inpatient_api/inpatient_api.dart';
 
 extension UserRMToDomain on UserRM {
-  User toDomain() {
+  User toDomainModel() {
     return User(
       sessionId: sessionId,
       username: username,
