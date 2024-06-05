fdd0cd255fa1c452f5e2d217a713905929c65cd7
yuzalsif <liteit68@gmail.com>
2024-06-05 14:59:51 +0300

    Add string representation for no cached base url

--- packages/inpatient_api/lib/src/base_url_storage.dart
+++ packages/inpatient_api/lib/src/base_url_storage.dart
@@ -2,13 +2,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class BaseUrlStorage {
   static String baseUrlStorageKey = 'BASE_URL_STORAGE_KEY';
+  static String noCachedBaseUrl = 'BASE URL HAVE NOT BEEN CACHED YET';
 
   static Future<SharedPreferences> _prefs() async {
     return await SharedPreferences.getInstance();
   }
 
   static Future<String> get baseUrl =>
-      _prefs().then((value) => value.getString(baseUrlStorageKey) ?? '');
+      _prefs().then((value) => value.getString(baseUrlStorageKey) ?? noCachedBaseUrl);
 
   static Future<void> setBaseUrl(String baseUrl) =>
       _prefs().then((value) => value.setString(baseUrlStorageKey, baseUrl));
