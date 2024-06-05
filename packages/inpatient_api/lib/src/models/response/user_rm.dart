29d90012dc92b49e38f3470aae7c2d29c5e6f907
yuzalsif <liteit68@gmail.com>
2024-06-05 14:58:25 +0300

    Modify fromJson method to match the server response

--- packages/inpatient_api/lib/src/models/response/user_rm.dart
+++ packages/inpatient_api/lib/src/models/response/user_rm.dart
@@ -12,8 +12,8 @@ class UserRM {
   factory UserRM.fromJson(Map<String, dynamic> json) {
     return UserRM(
       sessionId: json['sessionId'] as String,
-      username: json['username'] as String,
-      userUid: json['userUid'] as String,
+      username: json['user']['username'] as String,
+      userUid: json['user']['uuid'] as String,
     );
   }
 }
