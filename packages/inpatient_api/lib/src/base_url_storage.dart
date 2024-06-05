9cb6d3f2e699124a50a857288e81220cec24f21c
yuzalsif <liteit68@gmail.com>
2024-06-05 13:03:17 +0300

    Modify all base url storage methods to be static methods

--- packages/inpatient_api/lib/src/base_url_storage.dart
+++ packages/inpatient_api/lib/src/base_url_storage.dart
@@ -3,16 +3,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 class BaseUrlStorage {
   static String baseUrlStorageKey = 'BASE_URL_STORAGE_KEY';
 
-  Future<SharedPreferences> _prefs() async {
+  static Future<SharedPreferences> _prefs() async {
     return await SharedPreferences.getInstance();
   }
 
-  Future<String> get baseUrl =>
+  static Future<String> get baseUrl =>
       _prefs().then((value) => value.getString(baseUrlStorageKey) ?? '');
 
-  Future<void> setBaseUrl(String baseUrl) =>
+  static Future<void> setBaseUrl(String baseUrl) =>
       _prefs().then((value) => value.setString(baseUrlStorageKey, baseUrl));
 
-  Future<void> clearBaseUrl() =>
+  static Future<void> clearBaseUrl() =>
       _prefs().then((value) => value.remove(baseUrlStorageKey));
 }
