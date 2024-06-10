b38aef615b70f374c2a13cf203242930364bdb03
yuzalsif <liteit68@gmail.com>
2024-06-10 17:38:16 +0300

    Remove unused widgets

--- packages/component_library/lib/component_library.dart
+++ packages/component_library/lib/component_library.dart
@@ -1,6 +1,3 @@
-export 'src/ passwordtextfield.dart';
-export 'src/custom_button.dart';
-export 'src/custom_textfield.dart';
 export 'src/nurse_treatment_sheet.dart';
 export 'src/observation_chart_text_field.dart';
 export 'src/patient_card.dart';
