169a4b9586c97b1c920c33eff57c6e631b7122a8
yuzalsif <liteit68@gmail.com>
2024-06-22 00:17:38 +0300

    feat: Add investigation screen

---
+++ packages/features/investigation/lib/src/investigation_screen.dart
@@ -0,0 +1,49 @@
+import 'package:component_library/component_library.dart';
+import 'package:flutter/material.dart';
+
+class InvestigationScreen extends StatefulWidget {
+  const InvestigationScreen({super.key});
+
+  @override
+  State<InvestigationScreen> createState() => _InvestigationScreenState();
+}
+
+class _InvestigationScreenState extends State<InvestigationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text(
+          'Request Investigation',
+          style: TextStyle(
+            color: Color(0xFF1E1E1E),
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(
+          height: Spacing.xxLarge,
+        ),
+        const TextField(
+          decoration: InputDecoration(
+            labelText: 'Test',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(
+          height: Spacing.medium,
+        ),
+        const TextField(
+          decoration: InputDecoration(
+            labelText: 'Remarks',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(
+          height: Spacing.xxLarge,
+        ),
+        RoundFormButton(text: "Request", onPressed: () {})
+      ],
+    );
+  }
+}
