4d89cc7552973e30fc2ffc006884937c123eba06
yuzalsif <liteit68@gmail.com>
2024-06-22 13:29:45 +0300

    feat: Add nurse treatment sheet screen

---
+++ packages/features/nurse_treatment_sheet/lib/src/nurse_treatment_sheet_screen.dart
@@ -0,0 +1,100 @@
+import 'package:component_library/component_library.dart';
+import 'package:flutter/material.dart';
+
+class NurseTreatmentSheetScreen extends StatefulWidget {
+  const NurseTreatmentSheetScreen({super.key});
+
+  @override
+  State<NurseTreatmentSheetScreen> createState() => _NurseTreatmentSheetScreenState();
+}
+
+class _NurseTreatmentSheetScreenState extends State<NurseTreatmentSheetScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xFFF5F5F5),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16, top: 16, bottom: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Nurse Treatment Sheet',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xff1E1E1E))),
+                    SizedBox(
+                      height: Spacing.xLarge,
+                    ),
+                    _InputField(label: 'Oral Medication'),
+                    SizedBox(
+                      height: Spacing.xxxLarge,
+                    ),
+                    _InputField(label: 'Oral Infusion / Treatment'),
+                  ],
+                ),
+                Center(child: RoundFormButton(label: 'Save', onPressed: () {} )),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class _InputField extends StatefulWidget {
+  final String label;
+
+  const _InputField({super.key, required this.label});
+
+  @override
+  State<_InputField> createState() => _InputFieldState();
+}
+
+class _InputFieldState extends State<_InputField> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
+      width: double.infinity,
+      height: 100,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          top: 36,
+          bottom: 36,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20,
+              child: TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  label:  Text(
+                    widget.label,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xff8A8383),
+                    ),
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
