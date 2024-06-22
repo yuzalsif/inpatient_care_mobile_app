e5dfd6b0bda97a7d1ae70f4c845325ebd7aa6f41
yuzalsif <liteit68@gmail.com>
2024-06-22 23:47:18 +0300

    Export special_screen.dart

---
+++ packages/features/special/lib/src/special_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpecialScreen extends StatefulWidget {
+  const SpecialScreen({super.key});
+
+  @override
+  State<SpecialScreen> createState() => _SpecialScreenState();
+}
+
+class _SpecialScreenState extends State<SpecialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
