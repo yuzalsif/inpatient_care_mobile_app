b860c86bcf84f01f678905ce344c02eb8059a13b
yuzalsif <liteit68@gmail.com>
2024-06-25 10:45:34 +0300

    feat: Add assets

---
+++ lib/tablet_tab_container_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TabletTabContainerScreen extends StatefulWidget {
+  const TabletTabContainerScreen({super.key});
+
+  @override
+  State<TabletTabContainerScreen> createState() => _TabletTabContainerScreenState();
+}
+
+class _TabletTabContainerScreenState extends State<TabletTabContainerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: const [
+
+          ]
+        )
+      )
+    );
+  }
+}
