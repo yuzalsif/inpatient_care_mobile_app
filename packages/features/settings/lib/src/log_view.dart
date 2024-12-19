868f886e736759c0484a3365d0e42c2d3b62b9e5
Mdsoln <muddyfakih98@gmail.com>
2024-12-19 16:15:35 +0300

    added dynamic log view dependes on the state value of the setting

--- packages/features/settings/lib/src/log_view.dart
+++ packages/features/settings/lib/src/log_view.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'sms_setting.dart';
 
 
@@ -12,9 +12,34 @@ class LogViewScreen extends StatefulWidget {
 }
 
 class LogViewScreenState extends State<LogViewScreen> {
+  bool _enableIcareSMS = false;
+  String _phoneNumber = '';
+  String _serverUrl = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPreferences(); // Load saved preferences
+  }
+
+  Future<void> _loadPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _enableIcareSMS = prefs.getBool('enableIcareSMS') ?? false;
+      _phoneNumber = prefs.getString('phoneNumber') ?? '';
+      _serverUrl = prefs.getString('serveUrl') ?? '';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    String title = _enableIcareSMS && _phoneNumber.isNotEmpty
+        ? 'IcareSMS running ($_phoneNumber)'
+        : 'IcareSMS disabled';
+    String subtitle = _enableIcareSMS && _phoneNumber.isNotEmpty
+        ? 'New messages will be forwarded to server ($_serverUrl)'
+        : 'New messages will not be forwarded to server';
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(45.0),
@@ -25,9 +50,14 @@ class LogViewScreenState extends State<LogViewScreen> {
       body: Column(
         children: [
           GestureDetector(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SmsSetting()));
+            onTap: () async {
+              // Navigate to settings and wait for changes to be saved
+              await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SmsSetting()),
+              );
+              // Reload preferences after returning from settings
+              _loadPreferences();
             },
             child: Container(
               color: Colors.grey[600],
@@ -35,13 +65,13 @@ class LogViewScreenState extends State<LogViewScreen> {
                 child: Column(
                   children: [
                     Text(
-                      'IcareSMS disabled',
+                      title,
                       style: TextStyle(
                         fontSize: 15.0,
                       ),
                     ),
                     Text(
-                      'New messages will not be forwarded to server',
+                      subtitle,
                       style: TextStyle(
                         fontSize: 15.0,
                       ),
